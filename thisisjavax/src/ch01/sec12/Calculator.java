23b2a73a02b7da421ceefc02c5fcb93b6e2e4f21
sionkim <tkdlqhrm222@gmail.com>
2024-08-03 05:18:25 +0900

    first

---
+++ thisisjavax/src/ch01/sec12/Calculator.java
@@ -0,0 +1,17 @@
+package ch01.sec12;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = 
+				x + y;
+		System.out.println(result);
+		
+		// 4, 2, 4, 4, 3124, 4, ,4 //
+		// 4 2 4 3, 3124, 4, 1234, 
+
+	}
+
+}
